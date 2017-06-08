a3410f14efb114d82457a80105156434cc25d376
Likebard <1591127386@qq.com>
2017-06-08 11:26:41 +0800

    delete szh

--- szh-front/js/controllers/globalController.js
+++
@@ -1,8 +0,0 @@
-//定义作用于全局的控制器
-angular.module("mainApp")
-	.controller("globalCtrl", function($scope, $location) {
-		//页面跳转函数
-		$scope.jumptoUrl = function(path) {
-			$location.path(path);
-		}
-	});
