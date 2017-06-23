a92ab328d44b187243b4c0233b045054a6db487a
Likebard <1591127386@qq.com>
2017-06-23 22:00:19 +0800

    remove webApp

--- webApp/src/script/directive/head.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-//注意定义指令名要与模板中的指令名相同，-换成大写字母
-angular.module("app").directive("appHead", [function() {
-	return {
-		restrict: "A",
-		replace: true,
-		templateUrl: 'view/template/head.html'
-	};
-}]);
--- webApp/src/script/directive/positionList.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-angular.module('app').directive("appPositionList", [function() {
-	return {
-		restrict: "A", //该指令作为属性
-		replace: true, //是否替换DOM元素
-		templateUrl: 'view/template/positionList.html' //模板路径
-	}
-}])
